815d3f9fe319af361f36e108ff7e7464f1f40c8d
LeeHemingway <coherence0815@gmail.com>
2020-10-19 18:40:04 +0900

    do poc

---
+++ js/fvc.js
@@ -0,0 +1,138 @@
+(function(col) {
+
+  col.push({
+    "title": "Fruit Vitamin C",
+    "tag": "fvc",
+    "columns": ["繁體", "简体", "English", "日本語", "每100克含量 (mg/100g)", "種植", "食譜", "加工"], 
+    "data": [
+      {
+        "en": "Guava",
+        "tw": "番石榴",
+        "cn": "番石榴",
+        "ja": "石榴 (ざくろ)",
+        "num": 228.3,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Kiwifruit",
+        "tw": "奇異果",
+        "cn": "猕猴桃",
+        "ja": "キウイ",
+        "num": 92.7,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Longan",
+        "tw": "龍眼/桂圓",
+        "cn": "龙眼/桂圆",
+        "ja": "竜眼 (りゅうがん)",
+        "num": 84.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Papaya",
+        "tw": "木瓜",
+        "cn": "番木瓜",
+        "ja": "パパイヤ",
+        "num": 61.8,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Pomelo",
+        "tw": "柚子",
+        "cn": "柚",
+        "ja": "ザボン",
+        "num": 59.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Strawberry",
+        "tw": "草莓",
+        "cn": "草莓",
+        "ja": "苺 (いちご)",
+        "num": 58.8,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Oranges",
+        "tw": "柳丁",
+        "cn": "柳橙",
+        "ja": "ネーブル",
+        "num": 53.2,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Lemons",
+        "tw": "檸檬",
+        "cn": "柠檬",
+        "ja": "レモン",
+        "num": 53.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Pineapple",
+        "tw": "鳳梨",
+        "cn": "菠萝",
+        "ja": "パイナップル",
+        "num": 36.2,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Mandarin Orange",
+        "tw": "橘子",
+        "cn": "橘子",
+        "ja": "オレンジ",
+        "num": 26.7,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Dragon Fruit",
+        "tw": "火龍果",
+        "cn": "火龙果",
+        "ja": "ドラゴンフルーツ",
+        "num": 9.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Banana",
+        "tw": "香蕉",
+        "cn": "香蕉",
+        "ja": "バナナ",
+        "num": 8.7,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Green Apple",
+        "tw": "青蘋果",
+        "cn": "青苹果",
+        "ja": "青りんご",
+        "num": 4.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Grape",
+        "tw": "葡萄",
+        "cn": "葡萄",
+        "ja": "葡萄 (ぶどう)",
+        "num": 3.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }
+    ]
+  });
+
+})(collector);
---
+++ js/vvc.js
@@ -0,0 +1,182 @@
+(function(col) {
+
+  col.push({
+    "title": "Veg Vitamin C",
+    "tag": "vvc",
+    "columns": ["繁體", "简体", "English", "日本語", "每100克含量 (mg/100g)", "種植", "食譜", "加工"], 
+    "data": [
+      {
+        "en": "Bitter Cucumber",
+        "tw": "苦瓜",
+        "cn": "苦瓜",
+        "ja": "ツルレイシ",
+        "num": 120.0,
+        "plant": [
+          {
+            "name": "綠苦瓜種植教學",
+            "url": "https://youtu.be/NWQXF4fsMcw"
+          },{
+            "name": "綠苦瓜第2春教學",
+            "url": "https://youtu.be/gWT-9HiohTo"
+          }
+        ],
+        "cook": [
+          {
+            "name": "沖繩苦瓜雜炒",
+            "url": "https://youtu.be/vumnbJY4xCU"
+          }
+        ],
+        "process": []
+      }, {
+        "en": "Mustard Leaf",
+        "tw": "芥菜",
+        "cn": "芥菜",
+        "ja": "カラシナ",
+        "num": 94.0,
+        "plant": [],
+        "cook": [],
+        "process": [
+          {
+            "name": "醃製梅乾菜",
+            "url": "https://youtu.be/bcYYlL4xEbA"
+          },{
+            "name": "醃製酸菜",
+            "url": "https://youtu.be/WThI1AeCHbg"
+          }
+        ]
+      }, {
+        "en": "Sweet Pepper",
+        "tw": "甜椒",
+        "cn": "彩椒",
+        "ja": "ピーマン",
+        "num": 77.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Chinese Chive (Flower Stalk)",
+        "tw": "韭菜花",
+        "cn": "韭花",
+        "ja": "ハナニラ",
+        "num": 31.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Tomato",
+        "tw": "番茄",
+        "cn": "西紅柿",
+        "ja": "トマト",
+        "num": 24.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Winter Melon",
+        "tw": "冬瓜",
+        "cn": "冬瓜",
+        "ja": "トウガン",
+        "num": 24.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Chinese Chive",
+        "tw": "韭菜",
+        "cn": "韭菜",
+        "ja": "ニラ",
+        "num": 21.0,
+        "plant": [
+          {
+            "name": "韭菜移植教學",
+            "url": "https://youtu.be/jtF6WHs5pYQ"
+          }
+        ],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Chinese Spinach",
+        "tw": "莧菜",
+        "cn": "苋菜",
+        "ja": "アマランス",
+        "num": 15.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Radish",
+        "tw": "白蘿蔔",
+        "cn": "白萝卜",
+        "ja": "ダイコン",
+        "num": 13.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Cucumber",
+        "tw": "小黃瓜",
+        "cn": "黃瓜",
+        "ja": "胡瓜",
+        "num": 10.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Blanched Chinese Chive",
+        "tw": "韭黃",
+        "cn": "韭黄",
+        "ja": "きにら",
+        "num": 9.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Pumpkin",
+        "tw": "南瓜",
+        "cn": "南瓜",
+        "ja": "南瓜",
+        "num": 9.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Onion",
+        "tw": "洋蔥",
+        "cn": "洋葱",
+        "ja": "タマネギ",
+        "num": 7.4,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Carrot",
+        "tw": "紅蘿蔔",
+        "cn": "胡萝卜",
+        "ja": "人参",
+        "num": 5.9,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Angled Luffa",
+        "tw": "絲瓜",
+        "cn": "丝瓜",
+        "ja": "糸瓜",
+        "num": 5.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }, {
+        "en": "Eggplant",
+        "tw": "茄子",
+        "cn": "茄子",
+        "ja": "ナス",
+        "num": 3.0,
+        "plant": [],
+        "cook": [],
+        "process": []
+      }
+    ]
+  });
+
+})(collector);
