7f9226e22fd72971fdcb05d8091b5836321acb42
LeeHemingway <coherence0815@gmail.com>
2020-10-21 15:34:48 +0900

    add cancer data

---
+++ js/cancer.js
@@ -0,0 +1,19 @@
+(function(col, data) {
+
+  col.push({
+    "title": "Cancer",
+    "tag": "uspest",
+    "columns": ["繁體", "简体", "English", "日本語", "種植", "食譜", "加工"], 
+    "data": [
+      {
+        "d": data["_Strawberry_"]
+      }
+    ]
+  });
+
+})(collector, all_data);
+
+// 
+// Green tea Strawberries Blackberries Raspberries Blueberries Oranges Grapefruit Lemons Apples Pineapple Cherries 
+// Red grapes Red wine Bok Choy Kale Soy beans Ginseng Maitake mushroom Licorice Turmeric Nutmeg Artichokes 
+// Lavender Pumpkin Sea Cucumber Tuna Parsley Garlic Tomato Olive oil Grape seed oil Dark chocolate Others 
--- js/uspesticide.js
+++ js/uspesticide.js
@@ -1,19 +1,19 @@
 (function(col, data) {
 
-    col.push({
-      "title": "US Pesticide",
-      "tag": "uspest",
-      "columns": ["繁體", "简体", "English", "日本語", "種植", "食譜", "加工"], 
-      "data": [
-        {
-          "d": data["_Strawberry_"]
-        }, {
-          "d": data["_Spinach_"]
-        }, {
-          "d": data["_Kale_"]
-        }
-      ]
-    });
-  
-  })(collector, all_data);
+  col.push({
+    "title": "US Pesticide",
+    "tag": "uspest",
+    "columns": ["繁體", "简体", "English", "日本語", "種植", "食譜", "加工"], 
+    "data": [
+      {
+        "d": data["_Strawberry_"]
+      }, {
+        "d": data["_Spinach_"]
+      }, {
+        "d": data["_Kale_"]
+      }
+    ]
+  });
+
+})(collector, all_data);
   
