e2d5673896ecb7fe59941daf50afa1e65f4d97b8
rosemary0401 <rc760401@gmail.com>
2020-10-24 16:20:25 +0800

    Update vpesticide.js

--- js/vpesticide.js
+++ js/vpesticide.js
@@ -15,26 +15,26 @@
           "d": data["_Radish_"],
           "num": 16.7
         }, {
-          "d": data["_Radish_"],
-          "num": 16.7
+          "d": data["_Chili_"],
+          "num": 15.9
         }, {
-          "d": data["_Radish_"],
-          "num": 16.7
+          "d": data["_Chinese_Chive_"],
+          "num": 15.8
         }, {
-          "d": data["_Radish_"],
-          "num": 16.7
+          "d": data["_Chinese_kale_"],
+          "num": 14.8
         }, {
-          "d": data["_Radish_"],
-          "num": 16.7
+          "d": data["_Spinach_"],
+          "num": 14.6
         }, {
-          "d": data["_Radish_"],
-          "num": 16.7
+          "d": data["_Garland_Chrysanthemum_"],
+          "num": 14.3
         }, {
-          "d": data["_Radish_"],
-          "num": 16.7
+          "d": data["_Kidney_Bean_"],
+          "num": 14.1
         }, {
-          "d": data["_Radish_"],
-          "num": 16.7
+          "d": data["_Cowpea_"],
+          "num": 13.2
         }
       ]
     });
